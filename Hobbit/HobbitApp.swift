ea0f06c0f9614892a3d68856d25d61e22ebc0f4b
ritaqyang <ritaqyang@gmail.com>
2023-06-17 21:52:52 -0400

    Initial Commit

---
+++ Hobbit/HobbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbitApp.swift
+//  Hobbit
+//
+//  Created by Rita Yang on 2023-06-17.
+//
+
+import SwiftUI
+
+@main
+struct HobbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
